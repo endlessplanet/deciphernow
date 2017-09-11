c6f6b8f6ef73597a6bc7f86b229dc3be55caab2e
Emory Merryman <emory.merryman@gmail.com>
2017-09-11 11:38:51 +0000

    use user root for now

--- bin/exec.sh
+++ bin/exec.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-docker \
-    container \
-    create \
-    --cidfile ${PWD}/cidfile \
-    --interactive \
-    --tty \
-    --env ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" \
-    --env KNOWN_HOSTS="$(cat ${HOME}/.ssh/known_hosts)" \
-    --env LDAP_USERNAME="emory.merryman" \
-    --env LDAP_EMAIL="emory.merryman@deciphernow.com" \
-    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --user root \
-    endlessplanet/deciphernow &&
+cleanup(){
+    rm ${PWD}/cidfile
+} &&
+    trap cleanup EXIT &&
+    docker \
+        container \
+        create \
+        --cidfile ${PWD}/cidfile \
+        --interactive \
+        --tty \
+        --env ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" \
+        --env KNOWN_HOSTS="$(cat ${HOME}/.ssh/known_hosts)" \
+        --env LDAP_USERNAME="emory.merryman" \
+        --env LDAP_EMAIL="emory.merryman@deciphernow.com" \
+        --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+        --user root \
+        endlessplanet/deciphernow &&
     docker container start --interactive $(cat ${PWD}/cidfile) &&
     docker container stop $(cat ${PWD}/cidfile) &&
     docker container rm $(cat ${PWD}/cidfile) &&
--- image/root/run.sh
+++ image/root/run.sh
@@ -6,11 +6,11 @@ apk update &&
     apk add --no-cache sudo &&
     cp /opt/docker/user.sudo /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
-    mkdir /home/user/docker &&
-    mkdir /home/user/docker/containers &&
-    mkdir /home/user/docker/volumes &&
-    mkdir /home/user/docker/networks &&
-    chown -R user:user /home/user/docker &&
+    mkdir /root/docker &&
+    mkdir /root/docker/containers &&
+    mkdir /root/docker/volumes &&
+    mkdir /root/docker/networks &&
+#    chown -R user:user /root/docker &&
     apk add --no-cache util-linux &&
     apk add --no-cache bash &&
     rm -rf /var/cache/apk/*
