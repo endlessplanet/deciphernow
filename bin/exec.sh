57aa367f30f950a972e7866b7c75bdd947506c4d
Emory Merryman <emory.merryman@gmail.com>
2017-09-11 11:34:39 +0000

    use cidefile

--- bin/exec.sh
+++ bin/exec.sh
@@ -3,9 +3,15 @@
 docker \
     container \
     create \
+    --cidfile ${PWD}/cidfile \
+    --interactive \
+    --tty \
     --env ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" \
     --env KNOWN_HOSTS="$(cat ${HOME}/.ssh/known_hosts)" \
     --env LDAP_USERNAME="emory.merryman" \
     --env LDAP_EMAIL="emory.merryman@deciphernow.com" \
     endlessplanet/deciphernow &&
-    docker container start
+    docker container start --interactive $(cat ${PWD}/cidfile) &&
+    docker container stop $(cat ${PWD}/cidfile) &&
+    docker container rm $(cat ${PWD}/cidfile) &&
+    rm -f $(cat ${PWD}/cidfile)
