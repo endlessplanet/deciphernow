f8fbbad87df4c0dd62c9ff56d3e4977615cecea4
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 17:34:46 +0000

    fix build error

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
-    docker image build endlessplanet/deciphernow:$(git rev-parse --verify HEAD) image &&
+    docker image build --tag endlessplanet/deciphernow:$(git rev-parse --verify HEAD) image &&
     docker image push endlessplanet/deciphernow:$(git rev-parse --verify HEAD)
