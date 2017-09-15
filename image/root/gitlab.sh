5009ff793e63d54523deb34ab5f8793f01f8f1b6
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 20:05:41 +0000

    use --listen localhost to bypass auth

--- image/root/gitlab.sh
+++ image/root/gitlab.sh
@@ -11,7 +11,7 @@ PUBLIC_KEY_FILE=$(mktemp) &&
     CLOUD9_CIDFILE=$(mktemp ${HOME}/docker/containers/cloud9-XXXXXXXX) &&
     rm --force ${SHELL_CIDFILE} ${CLOUD9_CIDFILE} &&
     sudo docker container create --cidfile ${SHELL_CIDFILE} --volume /var/run/docker.sock:/var/run/docker.sock:ro fedora:26 sleep infinite &&
-    sudo docker container create --cidfile ${CLOUD9_CIDFILE} --volume /var/run/docker.sock:/var/run/docker.sock:ro sapk/cloud9 --listen 127.0.0.1 &&
+    sudo docker container create --cidfile ${CLOUD9_CIDFILE} --volume /var/run/docker.sock:/var/run/docker.sock:ro sapk/cloud9 --listen localhost &&
     sudo docker network connect ${NETWORK} $(cat ${SHELL_CIDFILE}) &&
     sudo docker network connect --alias ${ADVENTURE_NAME} ${NETWORK} $(cat ${CLOUD9_CIDFILE}) &&
 #    sudo docker container start ${SHELL_CIDFILE} &&
