18945f6b13a158b725e11dbb5589911c48453bc0
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 21:19:23 +0000

    the cloud9 client connects to sshd by itself but must be given the sshd container id

--- image/root/gitlab.sh
+++ image/root/gitlab.sh
@@ -11,14 +11,12 @@ ADVENTURE_NAME="${1}" &&
         container \
         create \
         --cidfile ${CLOUD9_CIDFILE} \
+        --env SSHD_CONTAINER=$(cat ${HOME}/docker/containers/sshd) \
         --volume /var/run/docker.sock:/var/run/docker.sock:ro \
         --volume $(cat ${HOME}/docker/volumes/workspace-${ADVENTURE_NAME}):/workspace/${ADVENTURE_NAME} \
         endlessplanet/cloud9:b14850c8fe1701dc5da91b91e3e5fceb84c4dde2 --listen localhost &&
-    sudo docker network connect ${NETWORK} $(cat ${SHELL_CIDFILE}) &&
     sudo docker network connect --alias ${ADVENTURE_NAME} ${NETWORK} $(cat ${CLOUD9_CIDFILE}) &&
-#    sudo docker container start ${SHELL_CIDFILE} &&
     sudo docker container start $(cat ${CLOUD9_CIDFILE}) &&
-    sudo docker container exec --interactive --tty ${CLOUD9_CIDFILE} cat /root/.ssh/id_rsa | sudo docker container exec --interactive $(cat ${HOME}/docker/containers/sshd) sh /opt/docker/reserve-ports.sh > ${HOME}/docker/ports/${ADVENTURE_NAME} &&
     sudo docker container exec --detach $(cat ${CLOUD9_CIDFILE}) ssh -fN -R 127.0.0.1:${SSHD_PORT}:127.0.0.1:8181 sshd &&
     sudo docker container exec --detach $(cat ${CLOUD9_CIDFILE}) ssh -fN -L 0.0.0.0:80:0.0.0.0:${SSHD_PORT} sshd &&
     # sudo docker container exec $(cat ${CLOUD9_CIDFILE}) ssh sshd &&
