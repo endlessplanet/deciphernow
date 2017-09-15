af40f436d382c73aab85df42f763d0a277a6e04c
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 18:55:50 +0000

    fix known_hosts problem

--- image/root/cloud9.sh
+++ image/root/cloud9.sh
@@ -21,10 +21,11 @@ PUBLIC_KEY_FILE=$(mktemp) &&
     sudo docker container exec --interactive --tty $(cat ${CLOUD9_CIDFILE}) apt-get install --assume-yes openssh-client &&
     sudo docker container exec --interactive --tty $(cat ${CLOUD9_CIDFILE}) ssh-keygen -f /root/.ssh/id_rsa -P "" &&
     sudo docker container exec --interactive --tty $(cat ${CLOUD9_CIDFILE}) cat /root/.ssh/id_rsa.pub | sudo docker container exec --interactive $(cat ${HOME}/docker/containers/sshd) tee --append /root/.ssh/authorized_keys &&
-    ssh-keyscan sshd | sudo docker container exec --interactive ${CLOUD9_CIDFILE)} tee /root/.ssh/known_hosts &&
+    sudo docker container exec --interactive --tty $(cat ${CLOUD9_CIDFILE}) ssh-keyscan sshd | sudo docker container exec --interactive ${CLOUD9_CIDFILE)} tee /root/.ssh/known_hosts &&
     sudo docker container exec --interactive --tty $(cat ${CLOUD9_CIDFILE}) chmod 0600 /root/.ssh/known_hosts &&
     SSHD_PORT=$(((${RANDOM}%10000)+10000)) &&
     sudo docker container exec $(cat ${CLOUD9_CIDFILE}) ssh -fN -R 127.0.0.1:${SSHD_PORT}:127.0.0.1:8181 sshd &&
     sudo docker container exec $(cat ${CLOUD9_CIDFILE}) ssh -fN -L 0.0.0.0:80:0.0.0.0:${SSHD_PORT} sshd &&
+    sudo docker container exec $(cat ${CLOUD9_CIDFILE}) apt-get install git &&
     # sudo docker container exec $(cat ${CLOUD9_CIDFILE}) ssh sshd &&
     true
