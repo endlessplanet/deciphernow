8ce551ef3e65225449f79c0fe36223f2bf0f3e41
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 18:02:52 +0000

    more work on cloud9.sh

--- image/root/cloud9.sh
+++ image/root/cloud9.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-SHELL_CIDFILE=$(mktemp ${HOME}/docker/containers/shell-XXXXXXXX) &&
+ADVENTURE_NAME="${1}" &&
+    SHELL_CIDFILE=$(mktemp ${HOME}/docker/containers/shell-XXXXXXXX) &&
     CLOUD9_CIDFILE=$(mktemp ${HOME}/docker/containers/cloud9-XXXXXXXX) &&
     rm --force ${SHELL_CIDFILE} ${CLOUD9_CIDFILE} &&
+    sudo docker container create --cidfile ${SHELL_CIDFILE} --volume /var/run/docker.sock:/var/run/docker.sock:ro fedora:26 &&
+    sudo docker container create --cidfile ${CLOUD9_CIDFILE} --volume /var/run/docker.sock:/var/run/docker.sock:ro sapk/cloud9 --auth user:password &&
+    sudo docker network connect ${NETWORK} $(cat ${SHELL_CIDFILE}) &&
+    sudo docker network connect --alias ${ADVENTURE_NAME} ${NETWORK} $(cat ${CLOUD9_CIDFILE}) &&
+#    sudo docker container start ${SHELL_CIDFILE} &&
+    sudo docker container start ${CLOUD9_CIDFILE} &&
     docker container create 
--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -11,8 +11,11 @@ cleanup() {
     trap cleanup EXIT &&
     sudo docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
     sudo docker image pull rastasheep/ubuntu-sshd:16.04 &&
+    sudo docker image pull fedora:26 &&
+    sudo docker image pull sapk/cloud9 &&
     mkdir ${HOME}/docker &&
     mkdir ${HOME}/docker/containers &&
     sudo docker container create --cidfile ${HOME}/docker/containers/sshd rastasheep/ubuntu-sshd:16.04 &&
+    sudo docker network connect --alias sshd ${NETWORK} $(cat ${HOME}/docker/containers/sshd) &&
     sudo docker container start $(cat ${HOME}/docker/containers/sshd)
     bash
