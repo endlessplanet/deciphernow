ed421611c4fa7712a58eee74d49acfaa2db42269
Emory Merryman <emory.merryman@gmail.com>
2017-09-15 21:30:19 +0000

    wtf

--- image/root/gitlab.sh
+++ image/root/gitlab.sh
@@ -14,10 +14,8 @@ ADVENTURE_NAME="${1}" &&
         --env SSHD_CONTAINER=$(cat ${HOME}/docker/containers/sshd) \
         --volume /var/run/docker.sock:/var/run/docker.sock:ro \
         --volume $(cat ${HOME}/docker/volumes/workspace-${ADVENTURE_NAME}):/workspace/${ADVENTURE_NAME} \
-        endlessplanet/cloud9:b14850c8fe1701dc5da91b91e3e5fceb84c4dde2 --listen localhost &&
+        endlessplanet/cloud9:b14850c8fe1701dc5da91b91e3e5fceb84c4dde2 &&
     sudo docker network connect --alias ${ADVENTURE_NAME} ${NETWORK} $(cat ${CLOUD9_CIDFILE}) &&
     sudo docker container start $(cat ${CLOUD9_CIDFILE}) &&
-    sudo docker container exec --detach $(cat ${CLOUD9_CIDFILE}) ssh -fN -R 127.0.0.1:${SSHD_PORT}:127.0.0.1:8181 sshd &&
-    sudo docker container exec --detach $(cat ${CLOUD9_CIDFILE}) ssh -fN -L 0.0.0.0:80:0.0.0.0:${SSHD_PORT} sshd &&
     # sudo docker container exec $(cat ${CLOUD9_CIDFILE}) ssh sshd &&
     true
