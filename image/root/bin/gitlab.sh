769e79661fc3ea4216c318719a00f3fb492fae82
Emory Merryman <emory.merryman@gmail.com>
2017-09-17 00:10:35 +0000

    do not use tty

--- image/root/bin/gitlab.sh
+++ image/root/bin/gitlab.sh
@@ -8,14 +8,14 @@ ADVENTURE_NAME="${1}" &&
     echo "${ORIGIN_ID_RSA}" | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/origin_id_rsa &&
     echo "${UPSTREAM_ID_RSA}" | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/upstream_id_rsa &&
     echo "${REPORT_ID_RSA}" | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/report_id_rsa &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) chmod 0600 /home/user/.ssh/report_id_rsa &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) chmod 0600 /home/user/.ssh/report_id_rsa &&
     sudo docker container cp /opt/docker/config.ssh.txt $(cat ${SHELL_CIDFILE}):/home/user/.ssh/config &&
-    ssh-keyscan gitlab.363-283.io | sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/known_hosts &&
+    ssh-keyscan gitlab.363-283.io | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/known_hosts &&
     sudo docker \
         container \
         exec \
         --interactive \
-        --tty \
+        \
         $(cat ${SHELL_CIDFILE}) \
             chmod 0600 \
             /home/user/.ssh/origin_id_rsa \
@@ -23,11 +23,11 @@ ADVENTURE_NAME="${1}" &&
             /home/user/.ssh/report_id_rsa \
             /home/user/.ssh/report_id_rsa \
             /home/user/.ssh/known_hosts &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} init &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add origin ssh://origin/${LDAP_USERNAME}/notification-engine.git &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add upstream ssh://origin/${ORGANIZATION}/${REPOSITORY}.git &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote set-url --push upstream no_push &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add report ssh://origin/${ORGNAIZATION}/${REPOSITORY}.git &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} config user.name "${LDAP_USERNAME}" &&
-    sudo docker container exec --interactive --tty $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} config user.email "${LDAP_EMAIL}"
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} init &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add origin ssh://origin/${LDAP_USERNAME}/notification-engine.git &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add upstream ssh://origin/${ORGANIZATION}/${REPOSITORY}.git &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote set-url --push upstream no_push &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} remote add report ssh://origin/${ORGNAIZATION}/${REPOSITORY}.git &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} config user.name "${LDAP_USERNAME}" &&
+    sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) git -C /workspace/${ADVENTURE_NAME} config user.email "${LDAP_EMAIL}"
     
