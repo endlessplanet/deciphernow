188bbede1241f5745eafc77f949a431550163460
Emory Merryman <emory.merryman@gmail.com>
2017-09-17 02:39:26 +0000

    do not use shell, just use cloud9

--- image/root/bin/gitlab.sh
+++ image/root/bin/gitlab.sh
@@ -4,6 +4,19 @@ ADVENTURE_NAME="${1}" &&
     ORGANIZATION="${2}" &&
     REPOSITORY="${3}" &&
     SHELL_CIDFILE=$(mktemp ${HOME}/docker/containers/shell-${ADVENTURE_NAME}-XXXXXXXX) &&
+    CLOUD9_CIDFILE=${SHELL_CIDFILE} &&
+    sudo docker volume create > ${HOME}/docker/volumes/workspace-${ADVENTURE_NAME} &&
+    rm --force ${SHELL_CIDFILE} &&
+    sudo \
+        docker \
+        container \
+        create \
+        --cidfile ${CLOUD9_CIDFILE} \
+        --env SSHD_CONTAINER=$(cat ${HOME}/docker/containers/sshd) \
+        --env SHELL_CONTAINER=$(cat ${SHELL_CIDFILE}) \
+        --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+        --volume $(cat ${HOME}/docker/volumes/workspace-${ADVENTURE_NAME}):/workspace/${ADVENTURE_NAME} \
+        endlessplanet/cloud9:8d446e5b98f4ca4344877fa079df32a08415fd0b 
     start-cloud9 "${ADVENTURE_NAME}" "${SHELL_CIDFILE}" &&
     echo "${ORIGIN_ID_RSA}" | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/origin_id_rsa &&
     echo "${UPSTREAM_ID_RSA}" | sudo docker container exec --interactive $(cat ${SHELL_CIDFILE}) tee /home/user/.ssh/upstream_id_rsa &&
