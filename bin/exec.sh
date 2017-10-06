e013c1c8a6a2b4af5ba1ee557c68e22ab0f0d54c
Emory Merryman <emory.merryman@gmail.com>
2017-10-06 00:04:16 +0000

    fix exec script

--- bin/exec.sh
+++ bin/exec.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
-CIDFILE=$(mktemp) &&
+NETW=$(mktemp) &&
+    DIND=$(mktemp) &&
+    WORK=$(mktemp) &&
     cleanup(){
-        docker container stop $(cat ${CIDFILE}) &&
-            docker container rm --volumes $(cat ${CIDFILE}) &&
-            rm -f ${CIDFILE}
+        docker container stop $(cat ${DIND}) $(cat ${WORK}) &&
+            docker container rm --volumes $(cat ${DIND}) $(cat ${WORK}) &&
+            docker network rm $(cat ${NETW}) &&
+            rm -f ${DIND} ${WORK} ${NETW}
     } &&
     trap cleanup EXIT &&
-    rm -f ${CIDFILE} &&
+    rm -f ${DIND} ${WORK} &&
     docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
+    docker image pull docker:17.09.0-dind &&
     docker image pull endlessplanet/deciphernow:$(git rev-parse --verify HEAD) &&
     docker \
         container \
         create \
-        --cidfile ${CIDFILE} \
+        --cidfile ${DIND} \
+        --privileged
+        docker:17.09.0-dind \
+            --host tcp://0.0.0.0:2376 &&
+    docker \
+        container \
+        create \
+        --cidfile ${WORK} \
         --interactive \
         --tty \
-        --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+        --env DOCKER_HOST=tcp://docker:2376
         endlessplanet/deciphernow:$(git rev-parse --verify HEAD) &&
-    docker container start --interactive $(cat ${CIDFILE})
+    docker network create $(uuidgen) > ${NETW} &&
+    docker network connect --alias docker $(cat ${NETW}) $(cat ${DIND}) &&
+    docker network connect $(cat ${WORK}) $(cat ${DIND}) &&
+    docker container start $(cat ${DIND}) &&
+    docker container start --interactive $(cat ${WORK})
