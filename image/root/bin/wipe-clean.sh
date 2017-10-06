b970c4e19ab866cc01fcaadc7f394194566f61dd
Emory Merryman <emory.merryman@gmail.com>
2017-10-06 02:21:46 +0000

    fix cleanup script

--- image/root/bin/wipe-clean.sh
+++ image/root/bin/wipe-clean.sh
@@ -2,7 +2,7 @@
 
 ls -1 ${HOME}/docker/containers | grep "^runner" | while read FILE
 do
-    echo docker container exec --interactive --tty $(cat ${HOME}/docker/containers/${FILE}) unregister --name proprietary
+    docker container exec --interactive --tty $(cat ${HOME}/docker/containers/${FILE}) unregister --name proprietary
 done &&
     ls -1 ${HOME}/docker/containers | while read FILE
     do
