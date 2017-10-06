689cfa3cfa9f3a10beaf00886f31f4f02c4a705b
Emory Merryman <emory.merryman@gmail.com>
2017-10-06 21:48:42 +0000

    remove ref to dind

--- image/root/bin/start-runner.sh
+++ image/root/bin/start-runner.sh
@@ -17,7 +17,5 @@ do
             --limit 1 \
             --url https://gitlab.363-283.io/ci \
             --executor docker \
-            --docker-host tcp://dind:2376 \
-            --docker-image docker:17.09.0-ce-dind \
             --docker-volumes /var/run/docker.sock:/var/run/docker.sock
 done
