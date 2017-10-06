51e5e77749cdb25a3fa479ba0d95b3c1101ed2ab
Emory Merryman <emory.merryman@gmail.com>
2017-10-06 20:54:12 +0000

    fix error in start runner script

--- image/root/bin/start-runner.sh
+++ image/root/bin/start-runner.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-gitlab-runner \
-    register \
-    --non-interactive \
-    --registration-token "${1}" \
-    --run-untagged \
-    --name "${2}" \
-    --limit 1 \
-    --url https://gitlab.363-283.io/ci \
-    --executor docker \
-    --docker-host tcp://dind:2376 \
-    --docker-image docker:17.09.0-ce-dind \
-    --docker-volumes /var/run/docker.sock:/var/run/docker.sock
+docker \
+    container \
+    exec \
+    --interactive \
+    --tty \
+    $(cat ${HOME}/docker/containers/gitlab-runner)
+        gitlab-runner \
+        register \
+        --non-interactive \
+        --registration-token "${1}" \
+        --run-untagged \
+        --name "${2}" \
+        --limit 1 \
+        --url https://gitlab.363-283.io/ci \
+        --executor docker \
+        --docker-host tcp://dind:2376 \
+        --docker-image docker:17.09.0-ce-dind \
+        --docker-volumes /var/run/docker.sock:/var/run/docker.sock
