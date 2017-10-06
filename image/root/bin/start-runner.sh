4e1f958073a4fd1f9b12c759ae896a2d389fd5af
Emory Merryman <emory.merryman@gmail.com>
2017-10-06 21:38:45 +0000

    multiple runners

--- image/root/bin/start-runner.sh
+++ image/root/bin/start-runner.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-docker \
-    container \
-    exec \
-    --interactive \
-    --tty \
-    $(cat ${HOME}/docker/containers/gitlab-runner) \
-        gitlab-runner \
-        register \
-        --non-interactive \
-        --registration-token "${1}" \
-        --run-untagged \
-        --name "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)" \
-        --limit 1 \
-        --url https://gitlab.363-283.io/ci \
-        --executor docker \
-        --docker-host tcp://dind:2376 \
-        --docker-image docker:17.09.0-ce-dind \
-        --docker-volumes /var/run/docker.sock:/var/run/docker.sock
+for REGISTRATION_TOKEN in "${@}"
+do
+    docker \
+        container \
+        exec \
+        --interactive \
+        --tty \
+        $(cat ${HOME}/docker/containers/gitlab-runner) \
+            gitlab-runner \
+            register \
+            --non-interactive \
+            --registration-token "${REGISTRATION_TOKEN}" \
+            --run-untagged \
+            --name "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)" \
+            --limit 1 \
+            --url https://gitlab.363-283.io/ci \
+            --executor docker \
+            --docker-host tcp://dind:2376 \
+            --docker-image docker:17.09.0-ce-dind \
+            --docker-volumes /var/run/docker.sock:/var/run/docker.sock
+done
