1523366550bb52d4dec00396596df7b3ad72b343
Emory Merryman <emory.merryman@gmail.com>
2017-11-09 15:15:43 +0000

    we do not need it

--- image/root/scripts/run.sh
+++ image/root/scripts/run.sh
@@ -7,11 +7,14 @@ apk update &&
     cp /opt/docker/etc/user.sudo.txt /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     mkdir /home/user/bin &&
-    ls -1 /opt/docker/bin | while read FILE
+    ls -1 /opt/docker/src/bin | while read FILE
     do
-        cp /opt/docker/bin/${FILE} /home/user/bin/${FILE%.*} &&
+        cp /opt/docker/src/bin/${FILE} /home/user/bin/${FILE%.*} &&
             chmod 0500 /home/user/bin/${FILE%.*}
     done &&
     chown -R user:user /home/user/bin &&
+    ls -1 /opt/docker/src/images | while read DIR
+    do
+    done &&
     ln -sf /home/user/bin/bashrc /home/user/.bashrc &&
     rm -rf /var/cache/apk/*
