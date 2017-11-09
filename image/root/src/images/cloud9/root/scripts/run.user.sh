7fcfd4018c62fe494749046c002eef605204e8af
Emory Merryman <emory.merryman@gmail.com>
2017-11-09 22:31:41 +0000

    fix volume order

--- image/root/src/images/cloud9/root/scripts/run.user.sh
+++ image/root/src/images/cloud9/root/scripts/run.user.sh
@@ -2,7 +2,7 @@
 
 cd ${HOME} &&
     git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1 &&
-    (cat > /home/user/.bashrc <<EOF
+    (cat >> /home/user/.bashrc <<EOF
 GIT_PROMPT_ONLY_IN_REPO=1
 source ~/.bash-git-prompt/gitprompt.sh
 EOF
