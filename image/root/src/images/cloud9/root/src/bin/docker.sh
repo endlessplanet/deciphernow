d7d21f1b0a650b69b9255872a2dd389a9c35389e
Emory Merryman <emory.merryman@gmail.com>
2017-11-09 20:14:32 +0000

    install latest docker

--- image/root/src/images/cloud9/root/src/bin/docker.sh
+++ image/root/src/images/cloud9/root/src/bin/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo /usr/bin/docker "${@}"
+sudo /usr/bin/docker container run --type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true docker:17.10.0 "${@}"
