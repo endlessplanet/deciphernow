61f29325d44d062187b7f191a26940fa4bd8fd74
Emory Merryman <emory.merryman@gmail.com>
2017-11-09 16:25:52 +0000

    use seconds since epoch for expiry

--- image/root/src/bin/build_images.sh
+++ image/root/src/bin/build_images.sh
@@ -3,5 +3,5 @@
 ls -1 /opt/docker/src/images | while read DIR
 do
     cd /opt/docker/src/images/${DIR} &&
-        docker image build --label expiry=$(date --date "now + 1 month") .
+        docker image build --label expiry=$(date --date "now + 1 month" +%s) .
 done
