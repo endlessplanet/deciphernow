2135f2a3da0cb2d7b255bbec430946b7dded2d40
Emory Merryman <emory.merryman@gmail.com>
2017-11-09 17:19:22 +0000

    create and start

--- image/root/src/bin/browser.sh
+++ image/root/src/bin/browser.sh
@@ -2,8 +2,9 @@
 
 docker \
     container \
-    run \
-    --detach \
+    create \
+    --cidfile browser.cid \
     --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
     --env DISPLAY \
-    $(docker image ls --quiet --filter label=title=browser | head -n 1)
+    $(docker image ls --quiet --filter label=title=browser | head -n 1) &&
+    docker container start $(cat browser.cid)
