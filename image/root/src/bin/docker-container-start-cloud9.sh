ba06bd462f936ea4ba74a1321ba505ea423bfa07
Emory Merryman <emory.merryman@gmail.com>
2017-11-10 16:26:27 +0000

    fix it

--- image/root/src/bin/docker-container-start-cloud9.sh
+++ image/root/src/bin/docker-container-start-cloud9.sh
@@ -9,6 +9,6 @@ then
             --cidfile ${HOME}/docker/containers/cloud9-${1} \
             --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
             $(docker image ls --quiet --filter label=title=cloud9 | head -n 1) &&
-        docker network connect --alias "${1}" $(cat ${HOME}/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${@}) &&
+        docker network connect --alias "${1}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${@}) &&
         docker container start $(cat ${HOME}/docker/containers/cloud9-${1})
 fi
