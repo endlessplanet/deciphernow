a37c4b6a18ffe26c1c69526107c35ad1614b14a9
Emory Merryman <emory.merryman@gmail.com>
2017-11-10 16:01:18 +0000

    fix cid to cidfile

--- image/root/src/bin/docker-container-start-cloud9.sh
+++ image/root/src/bin/docker-container-start-cloud9.sh
@@ -6,7 +6,7 @@ then
         docker \
             container \
             create \
-            --cid ${HOME}/docker/containers/cloud9-${@} \
+            --cidfile ${HOME}/docker/containers/cloud9-${@} \
             --mount --type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
             $(docker image ls --quiet --filter label=title=cloud9 | head -n 1) &&
         docker network connect --alias "${@}" $(cat ${HOME}/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${@}) &&
