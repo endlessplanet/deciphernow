1bbaa4af7a1a8c285b65e781e19fe33541645687
Emory Merryman <emory.merryman@gmail.com>
2017-11-10 16:39:40 +0000

    fix cloud9

--- image/root/src/images/cloud9/root/scripts/entrypoint.sh
+++ image/root/src/images/cloud9/root/scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node /opt/docker/c9sdk/server.js -w /opt/docker/workspace --auth user:password "${@}"
+node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace --auth user:password "${@}"
