dc2d4e1161d5dd677a89291f04804064d306298b
Emory Merryman <emory.merryman@gmail.com>
2017-11-10 16:22:47 +0000

    fix mount

--- image/root/src/bin/docker-container-start-cloud9.sh
+++ image/root/src/bin/docker-container-start-cloud9.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [ ! -f ${HOME}/docker/containers/cloud9-${@} ]
+if [ ! -f ${HOME}/docker/containers/cloud9-${1} ]
 then
     docker-container-start-browser &&
         docker \
             container \
             create \
-            --cidfile ${HOME}/docker/containers/cloud9-${@} \
-            --mount --type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
+            --cidfile ${HOME}/docker/containers/cloud9-${1} \
+            --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
             $(docker image ls --quiet --filter label=title=cloud9 | head -n 1) &&
-        docker network connect --alias "${@}" $(cat ${HOME}/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${@}) &&
-        docker container start $(cat ${HOME}/docker/containers/cloud9-${@})
+        docker network connect --alias "${1}" $(cat ${HOME}/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${@}) &&
+        docker container start $(cat ${HOME}/docker/containers/cloud9-${1})
 fi
