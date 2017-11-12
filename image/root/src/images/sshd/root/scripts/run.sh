7a65568d3be5821e95d4b046f54627369cc78684
Emory Merryman <emory.merryman@gmail.com>
2017-11-12 16:45:27 +0000

    use apk not apt-get

--- image/root/src/images/sshd/root/scripts/run.sh
+++ image/root/src/images/sshd/root/scripts/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-apt-get update --assume-yes &&
+apk update &&
+    apk upgrade &&
     sed -i "s,#GatewayPorts no,GatewayPorts yes," /etc/ssh/sshd_config &&
     mkdir /var/opt/docker &&
     echo $((${RANDOM}%10000+20000)) > /var/opt/docker/sshd.counter &&
     touch /root/.ssh/authorized_keys &&
     chmod 0600 /root/.ssh/authorized_keys &&
-    apt-get update --assume-yes &&
-    apt-get clean all
+    rm -rf /var/cache/apk/*
