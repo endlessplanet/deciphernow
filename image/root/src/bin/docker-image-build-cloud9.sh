fb08a76e0ac2e8a0772463f7500505da3ca1aa0b
Emory Merryman <emory.merryman@gmail.com>
2017-11-12 13:21:32 +0000

    use iidfile syntax

--- image/root/src/bin/docker-image-build-cloud9.sh
+++ image/root/src/bin/docker-image-build-cloud9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /opt/docker/src/images/cloud9 &&
-    docker image build --quiet --label expiry=$(date --date "now + 1 month" +%s) > ${HOME}/docker/images/cloud9
+    docker image build --label expiry=$(date --date "now + 1 month" +%s) --iidfile ${HOME}/docker/images/cloud9 .
