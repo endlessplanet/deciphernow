713f3bf4151d2e776fae9558ef08bf8535801b8c
Emory Merryman <emory.merryman@gmail.com>
2017-11-13 23:53:38 +0000

    fuck dollar one again

--- image/root/src/bin/docker-container-start-gitlab.sh
+++ image/root/src/bin/docker-container-start-gitlab.sh
@@ -25,7 +25,7 @@ done &&
             --cidfile ${HOME}/docker/containers/cloud9-${PROJECT_NAME} \
             --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
             $(cat ${HOME}/docker/images/cloud9) &&
-        docker network connect --alias "${1}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) &&
+        docker network connect --alias "${PROJECT_NAME}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) &&
         docker container start $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) &&
         ID_RSA_PUB=$(docker \
             container \
