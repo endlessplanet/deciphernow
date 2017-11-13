a90e04e8eadecd3ceab6aae6477889bf4c447bdd
Emory Merryman <emory.merryman@gmail.com>
2017-11-13 22:09:01 +0000

    fuck it

--- image/root/src/images/cloud9/root/src/sbin/tunnel2cloud9.sh
+++ image/root/src/images/cloud9/root/src/sbin/tunnel2cloud9.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ssh -i /root/.ssh/id_rsa -N -L 0.0.0.0:80:0.0.0.0:${1} sshd > /tmp/sshd2.log 2>&1
+ssh -i /root/.ssh/id_rsa -N -L 0.0.0.0:80:0.0.0.0:${1} sshd > /tmp/sshd2.log 2> /tmp/sshd2.err
+echo ${?} > /tmp/sshd2.code
--- image/root/src/images/cloud9/root/src/sbin/tunnel2sshd.sh
+++ image/root/src/images/cloud9/root/src/sbin/tunnel2sshd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ssh -i /root/.ssh/id_rsa -N -R 0.0.0.0:${1}:127.0.0.1:8181 sshd > /tmp/sshd1.log 2>&1
+ssh -i /root/.ssh/id_rsa -N -R 0.0.0.0:${1}:127.0.0.1:8181 sshd > /tmp/sshd1.log 2> /tmp/sshd1.err
+echo ${?} > /tmp/sshd1.code
