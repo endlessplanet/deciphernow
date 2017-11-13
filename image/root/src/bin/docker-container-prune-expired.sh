3485c7f1341573460ebc022e06ab523edf17c577
Emory Merryman <emory.merryman@gmail.com>
2017-11-13 18:54:03 +0000

    fuckery

--- image/root/src/bin/docker-container-prune-expired.sh
+++ image/root/src/bin/docker-container-prune-expired.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker container ls --quiet --filter label=expiry | while read CONTAINER
+docker container ls --quiet --all --filter label=expiry | while read CONTAINER
 do
     [ $(date +%s) -gt $(docker container inspect --format "{{index .Config.Labels \"expiry\"}}" ${CONTAINER}) ] &&
         docker container rm --volumes ${CONTAINER}
