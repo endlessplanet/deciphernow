94d6454d82ca6036ce8a07a033e3486868321adc
Emory Merryman <emory.merryman@gmail.com>
2017-11-13 21:44:03 +0000

    fucking ports

--- image/root/src/images/cloud9/root/src/sbin/tunnel2sshd.sh
+++ image/root/src/images/cloud9/root/src/sbin/tunnel2sshd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -i /root/.ssh/id_rsa -N -R 127.0.0.1:${1}:127.0.0.1:8181 sshd > /tmp/sshd1.log 2>&1
+ssh -i /root/.ssh/id_rsa -N -R 0.0.0.0:${1}:127.0.0.1:8181 sshd > /tmp/sshd1.log 2>&1
