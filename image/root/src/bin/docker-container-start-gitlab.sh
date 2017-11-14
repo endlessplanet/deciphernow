a28e7e1797199e832874c411a6ea52607a140983
Emory Merryman <emory.merryman@gmail.com>
2017-11-14 12:11:30 +0000

    do not use quotes

--- image/root/src/bin/docker-container-start-gitlab.sh
+++ image/root/src/bin/docker-container-start-gitlab.sh
@@ -83,6 +83,5 @@ done &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote add upstream ssh://upstream/${ORGANIZATION_NAME}/${PROJECT_NAME}.git &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote set-url --push upstream no_push &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote add origin ssh://origin/${LDAP_USERNAME}/${PROJECT_NAME}.git &&
-        docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) ln -sf /opt/docker/bin/post-commit /opt/docker/workspace/${PROJECT_NAME}/repo/.git/hooks &&
-        docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo scratch
+        docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) ln -sf /opt/docker/bin/post-commit /opt/docker/workspace/${PROJECT_NAME}/repo/.git/hooks
     fi
