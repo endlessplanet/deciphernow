7401b7c1248a4d292b0e3c11a2b0df982ba6ce98
Emory Merryman <emory.merryman@gmail.com>
2017-11-14 12:38:32 +0000

    install salt-master

--- image/root/src/images/cloud9/root/src/sbin/salt-master.sh
+++ image/root/src/images/cloud9/root/src/sbin/salt-master.sh
@@ -3,6 +3,7 @@
 yum update --assumeyes &&
     yum install --assumeyes procps-ng &&
     yum install --assumeyes curl &&
+    yum install --assumeyes salt-master &&
     ln --symbolic --force /workspace/saltstack/repo/srv/salt /srv/salt &&
     ln --symbolic --force /workspace/saltstack/repo/srv/pillar /srv/pillar &&
     mkdir /workspace &&
