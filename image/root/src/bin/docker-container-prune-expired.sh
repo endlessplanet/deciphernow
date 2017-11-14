2c6ca767c3a94dbe5a49e917fa2ec713d5744a07
Emory Merryman <emory.merryman@gmail.com>
2017-11-14 01:31:16 +0000

    stop expired containers

--- image/root/src/bin/docker-container-prune-expired.sh
+++ image/root/src/bin/docker-container-prune-expired.sh
@@ -3,5 +3,6 @@
 docker container ls --quiet --all --filter label=expiry | while read CONTAINER
 do
     [ $(date +%s) -gt $(docker container inspect --format "{{index .Config.Labels \"expiry\"}}" ${CONTAINER}) ] &&
+        docker container stop ${CONTAINER} &&
         docker container rm --volumes ${CONTAINER}
 done
