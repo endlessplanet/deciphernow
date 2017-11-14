11ac9eb8ad14d6f0631c956e5508167cac6a2814
Emory Merryman <emory.merryman@gmail.com>
2017-11-14 12:16:17 +0000

    wtf

--- image/root/src/bin/docker-container-start-gitlab.sh
+++ image/root/src/bin/docker-container-start-gitlab.sh
@@ -84,6 +84,7 @@ done &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote set-url --push upstream no_push &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote add origin ssh://origin/${LDAP_USERNAME}/${PROJECT_NAME}.git &&
         docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) ln -sf /opt/docker/bin/post-commit /opt/docker/workspace/${PROJECT_NAME}/repo/.git/hooks &&
-        export PATH=${PATH}:/opt/docker/bin &&
-        docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo scratch
+        # export PATH=${PATH}:/opt/docker/bin &&
+        # docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo scratch
+        true
     fi
