ecc83af9752e0108df625896d3098d09c94a5af0
Emory Merryman <emory.merryman@gmail.com>
2017-11-15 12:22:12 +0000

    add openssh

--- image/root/scripts/run.sh
+++ image/root/scripts/run.sh
@@ -2,7 +2,7 @@
 
 apk update &&
     apk upgrade &&
-    apk add --no-cache sudo bash coreutils util-linux &&
+    apk add --no-cache sudo bash coreutils util-linux openssh &&
     cp /opt/docker/etc/user.sudo.txt /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     adduser -D user &&
