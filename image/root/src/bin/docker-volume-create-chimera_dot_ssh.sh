c3dfc4aa0df704dcbc7f814552196bdc097f81dc
Emory Merryman <emory.merryman@gmail.com>
2017-11-15 13:06:28 +0000

    add user to sshd

--- image/root/src/bin/docker-volume-create-chimera_dot_ssh.sh
+++ image/root/src/bin/docker-volume-create-chimera_dot_ssh.sh
@@ -15,10 +15,12 @@ User ${LDAP_USERNAME}
 IdentityFile ~/.ssh/id_rsa
 ControlMaster auto
 ControlPath ~/.ssh/chm-ctrl_path-%H-%P-%R
+LocalForward 0.0.0.0:8080 s3.amazonaws.com:80
+LocalForward 0.0.0.0:8443  s3.amazonaws.com:443
 EOF
         ) | docker container run --interactive --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 tee user/.ssh/config &&
         docker container run --interactive --tty --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 chmod 0600 user/.ssh/id_rsa user/.ssh/config &&
         ssh-keyscan -p 2233 54.173.144.101 | docker container run --interactive --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 tee user/.ssh/known_hosts &&
         docker container run --interactive --tty --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 chmod 0644 user/.ssh/id_rsa user/.ssh/known_hosts &&
-        docker container run --interactive --tty --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 chown -R 500:500 user
+        docker container run --interactive --tty --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home --workdir /home alpine:3.4 chown -R 1000:1000 user
 fi
