d21414403261fa05f0673339bc583dd604e69a1e
Emory Merryman <emory.merryman@gmail.com>
2017-11-15 12:49:19 +0000

    add user to sshd

--- image/root/src/bin/docker-container-start-sshd.sh
+++ image/root/src/bin/docker-container-start-sshd.sh
@@ -9,7 +9,7 @@ then
             create \
             --cidfile ${HOME}/docker/containers/sshd \
             --label expiry=$(date --date "${EXPIRY}" +%s) \
-            --mount type=bind,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home,type=readonly \
+            --mount type=bind,source=$(cat ${HOME}/docker/volumes/chimera_dot_ssh),destination=/home,readonly=true \
             $(cat ${HOME}/docker/images/sshd) &&
             docker-network-start-regular &&
             docker network connect --alias sshd $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/sshd)
