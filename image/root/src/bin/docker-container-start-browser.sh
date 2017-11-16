53b5f4c9ac714c9e23c79e5c3711b32e760f4d1c
Emory Merryman <emory.merryman@gmail.com>
2017-11-16 17:07:24 +0000

    mount home to browser

--- image/root/src/bin/docker-container-start-browser.sh
+++ image/root/src/bin/docker-container-start-browser.sh
@@ -3,11 +3,13 @@
 if [ ! -f ${HOME}/docker/containers/browser ]
 then
     docker-image-build-browser &&
+        docker-volume-create-home &&
         docker \
             container \
             create \
             --cidfile ${HOME}/docker/containers/browser \
             --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+            --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home \
             --env DISPLAY \
             --label expiry=$(date --date "${EXPIRY}" +%s) \
             $(cat ${HOME}/docker/images/browser) &&
