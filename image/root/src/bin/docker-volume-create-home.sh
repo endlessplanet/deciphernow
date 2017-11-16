f05fcaa425824a42396a65c8c8118470df7cff44
Emory Merryman <emory.merryman@gmail.com>
2017-11-16 17:26:07 +0000

    fix errant brace

--- image/root/src/bin/docker-volume-create-home.sh
+++ image/root/src/bin/docker-volume-create-home.sh
@@ -18,7 +18,7 @@ then
         docker container run --interactive --tty --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home --workdir /home alpine:3.4 chown -R 1000:1000 user &&
         docker-image-build-cloud9 &&
         curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | docker container run --interactive --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home --env PROJECT_NAME=garbage --entrypoint bash $(cat ${HOME}/docker/images/cloud9) &&
-        docker container run --interactive --tty  --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home --entrypoint git --workdir /home/user $(cat $HOME}/docker/images/cloud9) clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1 &&
+        docker container run --interactive --tty  --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home --entrypoint git --workdir /home/user $(cat ${HOME}/docker/images/cloud9) clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1 &&
         cat /opt/docker/etc/bash_config.txt | docker container run --interactive --rm --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home --workdir /home/user alpine:3.4 tee --append .bashrc &&
         true
 fi
