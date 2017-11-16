656a27b65717b6cca5996f8b472e7dd87cbf5ad4
Emory Merryman <emory.merryman@gmail.com>
2017-11-16 20:04:00 +0000

    fix browser

--- image/root/src/bin/docker-container-start-cloud9.sh
+++ image/root/src/bin/docker-container-start-cloud9.sh
@@ -3,13 +3,13 @@
 while [ ${#} -gt 0 ]
 do
     case ${1} in
-        --project-name)
-            export PROJECT_NAME="${2}" &&
+        --host-name)
+            export HOST_NAME="${2}" &&
                 shift 2
         ;;
     esac
 done &&
-    if [ ! -f ${HOME}/docker/containers/cloud9-${PROJECT_NAME} ]
+    if [ ! -f ${HOME}/docker/containers/cloud9-${HOST_NAME} ]
     then
         docker-image-build-cloud9 &&
         docker-container-start-sshd &&
@@ -19,21 +19,21 @@ done &&
             container \
             create \
             --label expiry=$(date --date "${EXPIRY}" +%s) \
-            --cidfile ${HOME}/docker/containers/cloud9-${PROJECT_NAME} \
+            --cidfile ${HOME}/docker/containers/cloud9-${HOST_NAmE} \
             --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
             --mount type=volume,source=$(cat ${HOME}/docker/volumes/home),destination=/home \
-            --env "PROJECT_NAME=${PROJECT_NAME}" \
+            --env "HOST_NAME=${HOST_NAME}" \
             --env "MASTER_BRANCH=${MASTER_BRANCH}" \
             $(cat ${HOME}/docker/images/cloud9) &&
-        docker network connect --alias "${PROJECT_NAME}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) &&
-        docker container start $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) &&
+        docker network connect --alias "${HOST_NAME}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${HOST_NAME}) &&
+        docker container start $(cat ${HOME}/docker/containers/cloud9-${HOST_NAME}) &&
         ID_RSA_PUB=$(docker \
             container \
             exec \
             --interactive \
             --tty \
             --user root \
-            $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) \
+            $(cat ${HOME}/docker/containers/cloud9-${HOST_NAME}) \
                 sh \
                 /opt/docker/src/sbin/tunnel-init.sh) &&
         echo ID_RSA_PUB=${ID_RSA_PUB} &&
@@ -52,7 +52,7 @@ done &&
             exec \
             --detach \
             --user root \
-            $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) \
+            $(cat ${HOME}/docker/containers/cloud9-${HOST_NAME}) \
                 sh \
                 /opt/docker/src/sbin/tunnel2sshd.sh \
                 "${PORT}" &&
@@ -61,7 +61,7 @@ done &&
             exec \
             --detach \
             --user root \
-            $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) \
+            $(cat ${HOME}/docker/containers/cloud9-${HOST_NAME}) \
                 sh \
                 /opt/docker/src/sbin/tunnel2cloud9.sh \
                 "${PORT}"
--- image/root/src/images/cloud9/root/scripts/entrypoint.sh
+++ image/root/src/images/cloud9/root/scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir /opt/docker/workspace/${PROJECT_NAME} &&
-    node /opt/docker/c9sdk/server.js --listen 127.0.0.1 -w /opt/docker/workspace/${PROJECT_NAME} "${@}"
+mkdir /opt/docker/workspace/${HOST_NAME} &&
+    node /opt/docker/c9sdk/server.js --listen 127.0.0.1 -w /opt/docker/workspace/${HOST_NAME} "${@}"
