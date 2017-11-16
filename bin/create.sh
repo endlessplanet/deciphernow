b1962e7b9021fcfc05a173b299e51e577a81566a
Emory Merryman <emory.merryman@gmail.com>
2017-11-16 16:08:54 +0000

    fuckery

--- bin/create.sh
+++ bin/create.sh
@@ -12,6 +12,8 @@ docker \
     --env CHIMERA_ID_RSA="$(cat private/chimera_id_rsa)" \
     --env UPSTREAM_ID_RSA="$(cat private/upstream_id_rsa)" \
     --env ORIGIN_ID_RSA="$(cat private/origin_id_rsa)" \
+    --env UPSTREAM_GITHUB_ID_RSA="$(cat private/upstream_github_id_rsa)" \
+    --env ORIGIN_GITHUB_ID_RSA="$(cat private/upstream_github_id_rsa)" \
     --env MASTER_BRANCH=develop \
     --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
     --name deciphernow \
