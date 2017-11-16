145e715b28201033a828f4304b8a03a5158cbcd4
Emory Merryman <emory.merryman@gmail.com>
2017-11-16 15:15:04 +0000

    fix assumeyes error

--- image/root/src/images/cloud9/root/src/bin/git-deconflict.sh
+++ image/root/src/images/cloud9/root/src/bin/git-deconflict.sh
@@ -5,5 +5,5 @@ git refresh &&
     git diff --cached --exit-code &&
     [ -z "$(git ls-files --other --exclude-standard --directory)" ] &&
     git fetch upstream "${MASTER_BRANCH}" &&
-    git checkout -p "upstream/${MASTER_BRANCH}" &&
+    git checkout --patch "upstream/${MASTER_BRANCH}" &&
     git commit
