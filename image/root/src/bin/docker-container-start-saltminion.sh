cdde8baf907ce5b605cae254bdb9d64408d44dfe
Emory Merryman <emory.merryman@gmail.com>
2017-11-17 13:08:24 +0000

    start salt-master in detach

--- image/root/src/bin/docker-container-start-saltminion.sh
+++ image/root/src/bin/docker-container-start-saltminion.sh
@@ -9,8 +9,8 @@ do
         ;;
     esac
 done &&
-    export HOST_NAME=${MINION_NAME} &&
     docker-container-start-saltmaster &&
-    docker-container-create-cloud9 ${@} &&
+    export HOST_NAME=${MINION_NAME} &&
+    docker-container-start-cloud9 ${@} &&
     docker network connect --alias "${MINION_NAME}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${MINION_NAME}) &&
     docker network connect $(cat ${HOME}/docker/networks/salt) $(cat ${HOME}/docker/containers/cloud9-${MINION_NAME})
