388d3b8d1bb1db9bdeae6fb1c33ccaef090460bb
Emory Merryman <emory.merryman@gmail.com>
2017-11-17 12:58:31 +0000

    fix minion

--- image/root/src/bin/docker-container-start-gitlab.sh
+++ image/root/src/bin/docker-container-start-gitlab.sh
@@ -27,9 +27,9 @@ done &&
             docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote add upstream ssh://upstream.gitlab/${ORGANIZATION_NAME}/${PROJECT_NAME}.git &&
             docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote set-url --push upstream no_push &&
             docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo remote add origin ssh://origin.gitlab/${LDAP_USERNAME}/${PROJECT_NAME}.git &&
-            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo git fetch upstream ${MASTER_BRANCH} &&
-            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo git checkout upstream/${MASTER_BRANCH} &&
-            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo git checkout -b scratch/$(uuidgen) &&
+            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo fetch upstream ${MASTER_BRANCH} &&
+            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo checkout upstream/${MASTER_BRANCH} &&
+            docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) git -C /opt/docker/workspace/${PROJECT_NAME}/repo checkout -b scratch/$(uuidgen) &&
             docker container exec --interactive --tty $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) ln -sf /opt/docker/bin/post-commit /opt/docker/workspace/${PROJECT_NAME}/repo/.git/hooks &&
             true
     fi
--- image/root/src/bin/docker-container-start-saltmaster.sh
+++ image/root/src/bin/docker-container-start-saltmaster.sh
@@ -5,4 +5,3 @@ export ORGANIZATION_NAME=chimera &&
         docker-container-start-gitlab &&
         docker container exec --interactive --tty --user root $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) sh /opt/docker/src/sbin/salt-master.sh &&
         true
-    fi
