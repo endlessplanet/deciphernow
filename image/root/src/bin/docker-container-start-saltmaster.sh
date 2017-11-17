d3b6a7a066b72574eac49741322e8978e9749ff8
Emory Merryman <emory.merryman@gmail.com>
2017-11-17 13:03:51 +0000

    start salt-master in detach

--- image/root/src/bin/docker-container-start-saltmaster.sh
+++ image/root/src/bin/docker-container-start-saltmaster.sh
@@ -3,5 +3,6 @@
 export ORGANIZATION_NAME=chimera &&
     export PROJECT_NAME=saltstack &&
         docker-container-start-gitlab &&
-        docker container exec --interactive --tty --user root $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) sh /opt/docker/src/sbin/salt-master.sh &&
+        docker container exec --detach --user root $(cat ${HOME}/docker/containers/cloud9-${PROJECT_NAME}) sh /opt/docker/src/sbin/salt-master.sh &&
+        sleep 15s &&
         true
--- image/root/src/bin/docker-container-start-saltminion.sh
+++ image/root/src/bin/docker-container-start-saltminion.sh
@@ -11,7 +11,6 @@ do
 done &&
     export HOST_NAME=${MINION_NAME} &&
     docker-container-start-saltmaster &&
-    sleep 15s &&
     docker-container-create-cloud9 ${@} &&
     docker network connect --alias "${MINION_NAME}" $(cat ${HOME}/docker/networks/regular) $(cat ${HOME}/docker/containers/cloud9-${MINION_NAME}) &&
     docker network connect $(cat ${HOME}/docker/networks/salt) $(cat ${HOME}/docker/containers/cloud9-${MINION_NAME})
